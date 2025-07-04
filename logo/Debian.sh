4ceec9149215993b0c24c1d7a60f5a7c69d30e66
lazzy DiSa <dimasalinov07@gmail.com>
2025-07-04 15:36:28 +0300

    Create Debian.sh

---
+++ logo/Debian.sh
@@ -0,0 +1,12 @@
+name=$(cat /etc/os-release | grep -w NAME)
+user=$(whoami)
+host=$(cat /etc/hostname)
+
+echo ""
+echo -e "      @##0\    \033[33mSystem: \033[0m${name:6:-1}"
+echo -e "     @    #    \033[33mShell:  \033[0m${SHELL:9}"
+echo -e "    @  @  #    \033[33mUser:   \033[0m$user"
+echo -e "    @  \#/     \033[33mHost:   \033[0m$host"
+echo -e "     @         "
+echo -e "      0        "
+echo ""
