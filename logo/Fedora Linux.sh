0a0b5e037e0c091bbe8e50703729c1ed0a755d5c
lazzy DiSa <dimasalinov07@gmail.com>
2025-07-07 15:00:51 +0300

    Update Fedora Linux.sh

--- logo/Fedora Linux.sh
+++ logo/Fedora Linux.sh
@@ -3,9 +3,9 @@ user=$(whoami)
 host=$(cat /etc/hostname)
 
 echo ""
-echo "\033[34m     @###\    \033[33mSystem:  \033[0m${name:6:-1}"
-echo "\033[34m      #   0   \033[33mShell:   \033[0m${SHELL:9}"
-echo "\033[34m  /#####0     \033[33mUser:    \033[0m$user"
-echo "\033[34m  @   #       \033[33mHost:    \033[0m$host"
-echo "\033[34m  \###0       "
+echo -e "\033[34m     @###\    \033[33mSystem:  \033[0m${name:6:-1}"
+echo -e "\033[34m      #   0   \033[33mShell:   \033[0m${SHELL:9}"
+echo -e "\033[34m  /#####0     \033[33mUser:    \033[0m$user"
+echo -e "\033[34m  @   #       \033[33mHost:    \033[0m$host"
+echo -e "\033[34m  \###0       "
 echo ""
