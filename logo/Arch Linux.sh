23263dc66862f613a57210bc3f8ead6f40c29ec4
lazzy DiSa <dimasalinov07@gmail.com>
2025-07-07 14:59:58 +0300

    Update Arch Linux.sh

--- logo/Arch Linux.sh
+++ logo/Arch Linux.sh
@@ -5,7 +5,7 @@ host=$(cat /etc/hostname)
 echo ""
 echo -e "\033[36m      0       \033[33mSystem: \033[0m${name:6:-1} "
 echo -e "\033[36m     @#0      \033[33mShell:  \033[0m${SHELL:9} "
-echo -e "\033[36m    @/ \0     \033[33mUser:   \033[0m$user "
+echo -e "\033[36m    @/ \@     \033[33mUser:   \033[0m$user "
 echo -e "\033[36m   @#   #0    \033[33mHost:   \033[0m$host "
 echo -e "\033[36m  /       \   "
 echo ""
