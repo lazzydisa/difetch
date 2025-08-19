5d06758e9901814846af2912461609e3cb653dce
lazzy DiSa <dimasalinov07@gmail.com>
2025-08-19 21:16:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 gcc diftech/difetch.c -o diftech/difetch
 sudo mkdir /opt
-mv diftech difetch
+mv diftech difetch # i did that step cuz i did mistake in the name of directory with files :/
 sudo mv difetch /opt
 sudo ln -s /opt/difetch/difetch /usr/bin/difetch
