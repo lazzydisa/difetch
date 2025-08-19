4f6a214d193376d965cd85003a19bdb54388aa54
lazzy DiSa <dimasalinov07@gmail.com>
2025-08-19 12:19:18 +0300

    Delete main.sh

--- main.sh
+++
@@ -1,8 +0,0 @@
-name=$(cat /etc/os-release | grep -w NAME)
-
-# For now fhis construction is for Debian's peculiarities of the /etc/os-release file
-if [ ${name:0:6} = "Debian" ]; then
-    bash /opt/difetch/logo/Debian.sh
-else
-    bash /opt/difetch/logo/"${name:6:-1}.sh"
-fi
