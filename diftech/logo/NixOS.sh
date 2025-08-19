1c3ac057ffab15c687ede8f91a07c29765275301
lazzy DiSa <dimasalinov07@gmail.com>
2025-08-19 21:19:20 +0300

    Update NixOS.sh

--- diftech/logo/NixOS.sh
+++ diftech/logo/NixOS.sh
@@ -1,13 +1,17 @@
 name=$(cat /etc/os-release | grep -w NAME)
 user=$(whoami)
 host=$(cat /etc/hostname)
+desktop=$(echo $DESKTOP_SESSION)
+term=$(echo $TERM)
 
 # I know it's bad :/
 echo ""
-echo -e "\033[36m     0    0      \033[33mSystem: \033[0m${name:6:-1}"
-echo -e "\033[36m    0##   #0     \033[33mShell:  \033[0m${SHELL:9} "
-echo -e "\033[36m      @###@  0   \033[33mUser:   \033[0m$user"
-echo -e "\033[36m  0###0   0###0  \033[33mHost:   \033[0m$host"
+echo -e "   Welcome to \033[36m$host, \033[34m$user!"
+echo ""
+echo -e "\033[36m     0    0      \033[33mSystem:   \033[0m${name:6:-1}"
+echo -e "\033[36m    0##   #0     \033[33mShell:    \033[0m${SHELL:9} "
+echo -e "\033[36m      @###@  0   \033[33mWM/DE:    \033[0m${desktop}"
+echo -e "\033[36m  0###0   0###0  \033[33mTerminal: \033[0m${term} "
 echo -e "\033[36m   0  @###@      "
 echo -e "\033[36m     0#   ##0    "
 echo -e "\033[36m      0    0     "
