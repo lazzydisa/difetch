e56edde1f73a429985e96db0363566e619a2275d
lazzy DiSa <dimasalinov07@gmail.com>
2025-08-19 12:01:58 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ name=$(cat /etc/os-release | grep -w NAME)
 
 # For now fhis construction is for Debian's peculiarities of the /etc/os-release file
 if [ ${name:0:6} = "Debian" ]; then
-    bash logo/Debian.sh
+    bash /opt/difetch/logo/Debian.sh
 else
-    bash logo/"${name:6:-1}.sh"
+    bash /opt/difetch/logo/"${name:6:-1}.sh"
 fi
