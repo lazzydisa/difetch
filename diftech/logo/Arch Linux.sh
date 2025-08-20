f5d068c31cf013020caa47cc88e301e426dd1fa1
lazzy DiSa <dimasalinov07@gmail.com>
2025-08-20 22:00:25 +0300

    Update Arch Linux.sh

--- diftech/logo/Arch Linux.sh
+++ diftech/logo/Arch Linux.sh
@@ -1,15 +1,13 @@
 name=$(cat /etc/os-release | grep -w NAME)
 user=$(whoami)
 host=$(cat /etc/hostname)
-desktop=$(echo $DESKTOP_SESSION)
-term=$(echo $TERM)
 
 echo ""
 echo -e "   Welcome to \033[36m$host, \033[37m$user!"
 echo ""
 echo -e "\033[36m      0       \033[33mSystem:   \033[0m${name:6:-1}  "
 echo -e "\033[36m     @#0      \033[33mShell:    \033[0m${SHELL:9}    "
-echo -e "\033[36m    @/ \@     \033[33mWM/DE:    \033[0m${desktop}  "
-echo -e "\033[36m   @#   #0    \033[33mTerminal: \033[0m${term}  "
+echo -e "\033[36m    @/ \@     \033[33mWM/DE:    \033[0m$DESKTOP_SESSION  "
+echo -e "\033[36m   @#   #0    \033[33mTerminal: \033[0m$TERM  "
 echo -e "\033[36m  /       \   "
 echo ""
