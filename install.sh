2b5c33f1d4035ab9f92f1a44243a2f75d98b9319
lazzy DiSa <dimasalinov07@gmail.com>
2025-08-21 20:58:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 gcc difetch/difetch.c -o difetch/difetch
-sudo mkdir /opt
-sudo mv difetch /opt
+sudo mkdir -p /opt/difetch
+sudo mv difetch/* /opt/difetch
 sudo ln -s /opt/difetch/difetch /usr/bin/difetch
