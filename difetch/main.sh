37911e2a06a6419415a4461ce4f2158b480639a2
lazzy DiSa <dimasalinov07@gmail.com>
2025-08-21 20:43:55 +0300

    Create main.sh

---
+++ difetch/main.sh
@@ -0,0 +1,21 @@
+# inports
+source variables.sh
+
+if [ $distro = "Debian.sh" ]; then
+    source /opt/difetch/logo/Debian.sh
+else
+    source /opt/difetch/logo/$distro.sh
+fi
+
+# output
+echo ""
+echo -e "   Welcome to \033[35m$host, \033[36m$user!"
+echo ""
+echo -e "$s1    \033[33mOS:         \033[37m$distro"
+echo -e "$s2    \033[33mShell:      \033[37m$shell"
+echo -e "$s3    \033[33mWM/DE:      \033[37m$desktop"
+echo -e "$s4    \033[33mTerminal:   \033[37m$term"
+echo -e "$s5    "
+echo -e "$s6    "
+echo -e "$s7    "
+echo ""
